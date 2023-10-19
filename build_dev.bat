4d14fd4e1cb6de4e34eea8f1f12313f359639594
tuntematonjr <mika.lieto@hotmail.com>
2023-10-20 00:26:31 +0300

    Now using HEMTT

---
+++ build_dev.bat
@@ -0,0 +1,2 @@
+@echo off
+hemtt dev -v
---
+++ mod.cpp
@@ -0,0 +1,27 @@
+dir = "@TunCon";
+name = "Tun Unconscious Info v0.0.0";
+
+author = "Tuntematon";
+
+picture         = "data\afilogo.paa";           // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                  // Hide the extension name
+hidePicture     = "false";                  // Hide the extension menu
+
+logoSmall       = "data\afilogo.paa";         // Display next to the item added by the mod 64x64
+logo            = "data\afilogo.paa";        // Logo displayed in the main menu 128x128
+logoOver        = "data\afilogo.paa"; // When the mouse is over, in the main menu 128x128
+tooltip         = "More info for unconscious";
+tooltipOwned    = "More info for unconscious";  // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "Tun Unconscious Info v0.0.0";         // Overview text, displayed from the extension menu
+description     = "More info for unconscious"; // Probably in context with action
+action          = "https://github.com/tuntematonjr/TunCon_Info";    // Website URL, that can accessed from the expansions menu 
+actionName      = "Github";                 // label of button/tooltip in extension menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    1,
+    0.0,
+    0.86,
+    1
+};
