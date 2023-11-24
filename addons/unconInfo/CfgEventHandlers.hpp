5032c5d4f3fb4c045a5b3b2cc24c10ae27abe356
tuntematonjr <mika.lieto@hotmail.com>
2023-11-24 18:50:20 +0200

    Remove test hint and minor cleaning

--- addons/unconInfo/CfgEventHandlers.hpp
+++ addons/unconInfo/CfgEventHandlers.hpp
@@ -1,12 +1,18 @@
-class Extended_PostInit_EventHandlers {
-    class Tun_unconInfo {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_clientInit));
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+class Extended_PreStart_EventHandlers {
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_preStart));
     };
 };
 
 class Extended_PreInit_EventHandlers {
-    class Tun_unconInfo {
+    class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_preInit));
+    };
+};
+
+class Extended_PostInit_EventHandlers {
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+        clientInit = QUOTE(call COMPILE_FILE(XEH_postInit_client));
     };
 };
--- addons/unconInfo/functions/fnc_hideMessage.sqf
+++ addons/unconInfo/functions/fnc_hideMessage.sqf
@@ -10,11 +10,10 @@
  * None
  *
  * Example:
- * [] call tun_unconInfo_fnc_hideMessage
+ * [] call tuncon_unconinfo_fnc_hideMessage
  */
 #include "script_component.hpp"
 
 if (GVAR(enableUnconInfo) && player getVariable ["ACE_isUnconscious", false]) then {
-	hint "test";
 	cutText ["", "PLAIN NOFADE", -1, false, true];
 };
