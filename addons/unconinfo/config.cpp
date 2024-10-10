d8a23a5979301ff5bb00fed3ba4c9d0071b7099f
tuntematonjr <mika.lieto@hotmail.com>
2024-10-10 11:34:33 +0300

    update required addons

--- addons/unconinfo/config.cpp
+++ addons/unconinfo/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {MAIN_ADDON_STR};
+        requiredAddons[] = {MAIN_ADDON_STR, "ace_medical_feedback", "ace_medical", "ace_medical_status", "ace_common"};
         author = "Tuntematon";
         authorUrl = GITHUBLINK;
         //VERSION_CONFIG;
